04ab301ce07649b660f8ce5ef469bb6c903a68a3
Kelsey Oshiro <kelseymosh@gmail.com>
2021-04-15 11:23:45 -0600

    Connected MongoDB

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
+const MongoClient = require('mongodb').MongoClient
+const connectionString = 'mongodb+srv://aparkmentsquad:beerolympics@cluster0.pcdst.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+MongoClient.connect(connectionString, {
+    useUnifiedTopology: true })
+        .then(client => {
+            console.log('Connected to Database')
+            const db = client.db('aparkment-data')
+        })
+        .catch(error => console.error(error))
+
+
 
 app.get('/', (request, response) => {
     response.send("Hello world")
 })
 
 app.listen(4000, () => console.log("listening on port 4000"))
+
